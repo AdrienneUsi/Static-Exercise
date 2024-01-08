0459f92c1e18ed7dd5b694d240a98986c7044ae0
AdrienneUsi <usiadrienne35@gmail.com>
2024-01-08 11:58:50 -0800

    finish w/ exercise

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine($"Your temp in Fahrenheit is {TempConverter.CelsiusToFahrenheit(20)}");
+            Console.WriteLine($"Your temp in Celsius is {TempConverter.FahrenheitToCelsius(20)}");
         }
     }
 }
